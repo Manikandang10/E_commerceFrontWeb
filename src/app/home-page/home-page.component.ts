bc513aacbbdb90008e29d5626dc14f04a1f8bb99
Manikandan G <gmanikandan149@gmail.com>
2024-12-01 01:43:25 +0530

    Add Home Page and Scroll Bar

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageComponent {
 
